34ee41d3abfbf6758645534a9521c4e4bc2a1f65
Gabor Peredi <peredi.g@gmail.com>
2021-04-17 12:49:35 +0200

    all function ok, highlight, without validation

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var rate = document.getElementById("rate").value;
 var years = document.getElementById("years").value;
 var interest = principal * years * rate / 100;
 var year = new Date().getFullYear()+parseInt (years);
-document.getElementById("result").innerHTML= "If you deposit "+principal+", \<br\>at an interest rate "+rate+" %, \<br\>You will receive an amount of "+interest+", \<br\>in the year "+year+" \<br\>"
+document.getElementById("result").innerHTML= "If you deposit <span class=highlight>"+principal+"</span>, \<br\>at an interest rate <span class=highlight>"+rate+"</span> %, \<br\>You will receive an amount of <span class=highlight>"+interest+"</span>, \<br\>in the year <span class=highlight>"+year+"</span> \<br\>"
 }
 function updateRate() {
 	var rateval = document.getElementById("rate").value;
