1d92d580c3768bc8654f8783959d6aeac1f8ee26
Gabor Peredi <peredi.g@gmail.com>
2021-04-19 18:56:38 +0200

    finished, design ok, functions work

--- script.js
+++ script.js
@@ -4,7 +4,15 @@ var rate = document.getElementById("rate").value;
 var years = document.getElementById("years").value;
 var interest = principal * years * rate / 100;
 var year = new Date().getFullYear()+parseInt (years);
+if (principal <= 0) {
+	alert("Enter a positive number");
+	document.getElementById("principal").focus();
+	return false;
+
+	}
+ else {
 document.getElementById("result").innerHTML= "If you deposit <span class=highlight>"+principal+"</span>, \<br\>at an interest rate <span class=highlight>"+rate+"</span> %, \<br\>You will receive an amount of <span class=highlight>"+interest+"</span>, \<br\>in the year <span class=highlight>"+year+"</span> \<br\>"
+}
 }
 function updateRate() {
 	var rateval = document.getElementById("rate").value;
